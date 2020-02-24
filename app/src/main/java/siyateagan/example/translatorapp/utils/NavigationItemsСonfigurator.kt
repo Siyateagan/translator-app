8ec03afa2ba2bab2ff03a6c012ccead050d22c12
Siyateagan <Siyateagan@users.noreply.github.com>
2020-02-24 14:33:52 +0300

    Import and replace bottom_nav_menu icons

--- app/src/main/java/siyateagan/example/translatorapp/utils/NavigationItemsСonfigurator.kt
+++ app/src/main/java/siyateagan/example/translatorapp/utils/NavigationItemsСonfigurator.kt
@@ -15,21 +15,21 @@ import siyateagan.example.translatorapp.ui.favorites.FavoritesActivity
 fun setItemsListeners(navView: BottomNavigationView, context: Context, activityName: String) {
     navView.setOnNavigationItemSelectedListener {
         when (it.itemId) {
-            R.id.navigation_home -> {
+            R.id.navigation_text_translation -> {
                 if (activityName != TextTranslationActivity::class.java.simpleName){
                     val intent = Intent(context, TextTranslationActivity::class.java)
                     context.startActivity(intent)
                 }
             }
 
-            R.id.navigation_dashboard -> {
+            R.id.navigation_voice_translation -> {
                 if (activityName != VoiceTranslationActivity::class.java.simpleName) {
                     val intent = Intent(context, VoiceTranslationActivity::class.java)
                     context.startActivity(intent)
                 }
             }
 
-            R.id.navigation_notifications -> {
+            R.id.navigation_favorites -> {
                 if (activityName != FavoritesActivity::class.java.simpleName) {
                     val intent = Intent(context, FavoritesActivity::class.java)
                     context.startActivity(intent)
