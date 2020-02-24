b9f7195af9e052e29b1ee9d5a8c5f60ce0beda16
Siyateagan <Siyateagan@users.noreply.github.com>
2020-02-24 16:00:25 +0300

    Set custom toolbar

--- app/src/main/java/siyateagan/example/translatorapp/FirstEntryActivity.kt
+++ app/src/main/java/siyateagan/example/translatorapp/FirstEntryActivity.kt
@@ -2,7 +2,9 @@ package siyateagan.example.translatorapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.toolbar.*
 import siyateagan.example.translatorapp.utils.setItemsListeners
 
 class FirstEntryActivity : AppCompatActivity() {
@@ -10,8 +12,9 @@ class FirstEntryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_entry)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        setSupportActionBar(toolbar)
 
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         setItemsListeners(navView, this, this::class.java.simpleName)
     }
 }
