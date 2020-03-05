9b235a39011116f8d74e14540aace237d33d67e3
Siyateagan <Siyateagan@users.noreply.github.com>
2020-03-05 10:56:37 +0300

    Rename RecyclerAdapter and item_history

--- app/src/main/java/siyateagan/example/translatorapp/ui/adapters/LanguagesAdapter.kt
+++ app/src/main/java/siyateagan/example/translatorapp/ui/adapters/LanguagesAdapter.kt
@@ -4,11 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.recycler_item.view.*
+import kotlinx.android.synthetic.main.item_language.view.*
 import siyateagan.example.translatorapp.R
 
-class RecyclerAdapter(private val myDataset: List<String>) :
-    RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>() {
+class LanguagesAdapter(private val myDataset: List<String>) :
+    RecyclerView.Adapter<LanguagesAdapter.MyViewHolder>() {
 
     class MyViewHolder(val item: View) : RecyclerView.ViewHolder(item)
 
@@ -17,12 +17,12 @@ class RecyclerAdapter(private val myDataset: List<String>) :
         viewType: Int
     ): MyViewHolder {
         val item = LayoutInflater.from(parent.context)
-            .inflate(R.layout.recycler_item, parent, false)
+            .inflate(R.layout.item_language, parent, false)
         return MyViewHolder(item)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.item.input_language.text = myDataset[position]
+        holder.item.language.text = myDataset[position]
     }
 
     override fun getItemCount() = myDataset.size
