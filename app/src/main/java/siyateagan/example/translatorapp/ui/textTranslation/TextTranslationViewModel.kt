e64f0786fd1ab155d938ea3481ab98177e5ce154
Siyateagan <48732103+Siyateagan@users.noreply.github.com>
2020-04-06 16:31:37 +0300

    Add swap of text input and output fields

--- app/src/main/java/siyateagan/example/translatorapp/ui/textTranslation/TextTranslationViewModel.kt
+++ app/src/main/java/siyateagan/example/translatorapp/ui/textTranslation/TextTranslationViewModel.kt
@@ -2,6 +2,7 @@ package siyateagan.example.translatorapp.ui.textTranslation
 
 import android.content.Intent
 import android.content.SharedPreferences
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
 import io.reactivex.schedulers.Schedulers
@@ -21,7 +22,7 @@ class TextTranslationViewModel @Inject constructor(
     var currentButton = LanguageButton.createCurrentButton(sharedPref, stringsHelper)
     var targetButton = LanguageButton.createTargetButton(sharedPref, stringsHelper)
 
-    var textToTranslate: String? = null
+    var textToTranslate: ObservableField<String?> = ObservableField("")
 
     @Inject
     lateinit var translateObserver: TranslateObserver
@@ -55,12 +56,21 @@ class TextTranslationViewModel @Inject constructor(
     }
 
     fun swapLanguages() {
+        swapLanguagesData()
+        swapButtons()
+    }
+
+    private fun swapLanguagesData() {
         currentButton.language =
             targetButton.language.also { targetButton.language = currentButton.language }
         currentButton.languageCode = targetButton.languageCode.also {
             targetButton.languageCode = currentButton.languageCode
         }
+        textToTranslate.set(translateObserver.translatedText.get()
+            .also { translateObserver.translatedText.set(textToTranslate.get()) })
+    }
 
+    private fun swapButtons() {
         val buttonsList = listOf(currentButton, targetButton)
         buttonsList.forEach {
             it.language.notifyChange()
@@ -69,13 +79,13 @@ class TextTranslationViewModel @Inject constructor(
     }
 
     fun translateText() {
-        if (textToTranslate.isNullOrBlank()) {
+        if (textToTranslate.get().isNullOrBlank()) {
             translateObserver.translatedText.set("")
             return
         }
         val translateDirection = "${currentButton.languageCode}-${targetButton.languageCode}"
 
-        yandexService.translate(textToTranslate!!, translateDirection)
+        yandexService.translate(textToTranslate.get()!!, translateDirection)
             .subscribeOn(Schedulers.io())
             .observeOn(mainThread())
             .subscribe(translateObserver)
